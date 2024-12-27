88d0e633b08893b43b1ed327fc447cdbcd7359ce
Julien Huang <julien.h.dev@gmail.com>
2024-12-27 14:04:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Repro Nuxt workspaces + vitest workspace
+
+- install `pnpm i`
+- run `npx vitest`
+
+--> context conflict
---
+++ projects/a/tests/a.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("a", () => {
+  it("a", () => {
+    expect(1).toBe(1);
+  });
+});
